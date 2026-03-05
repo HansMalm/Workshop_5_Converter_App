3d864e3233a8de5b129c82a38b14e09d977df5eb
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-03-05 10:39:46 +0100

    modify Main.java file
    add menu text

--- src/main/java/Lexicon/Hans/Main.java
+++ src/main/java/Lexicon/Hans/Main.java
@@ -1,16 +1,25 @@
 package Lexicon.Hans;
 
+import java.util.Scanner;
 
 public class Main {
     static void main() {
 
-        System.out.println("Welcome to The Converter!");
+        Scanner scanner = new Scanner(System.in);
+        int select;
+        System.out.println("\nWelcome to The Converter!");
 
 
         while (true) {
 
-            System.out.println("Choose a unit to convert from the list.");
-            System.out.println("");
+            System.out.println("\nChoose a unit to convert from the list.");
+            System.out.println("1 Temperature: Celsius to Fahrenheit.");
+            System.out.println("2 Data Storage: MB to GB.");
+            System.out.println("3 Hours to minutes.");
+            System.out.print("Choose option: ");
+            select = scanner.nextInt();
+
+
         }
     }
 }
